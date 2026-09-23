fd05f91ecb63d32654c8d9d4cd63ee8e77721547
teneraphael <teneraphael57@gmail.com>
2026-09-23 11:42:32 +0200

    mobile: register authentication route

--- dealcity-mobile/app/_layout.tsx
+++ dealcity-mobile/app/_layout.tsx
@@ -50,7 +50,8 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="auth" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
   );
